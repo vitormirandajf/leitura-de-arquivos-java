56f45e87d7c8ac8c817b62143a2b9ffc4f090ed1
vitormirandajf <vitor.miranda@viannasempre.com.br>
2023-07-03 15:13:45 -0300

    aula 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+
+        File file = new File("c:\\temp\\test.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e) {
+            System.out.println("Erro: "+ e.getMessage());
+        }finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
+
     }
 }
