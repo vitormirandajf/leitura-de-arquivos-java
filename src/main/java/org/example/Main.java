e4c98fc16470fd81b9dbc28d0e320506df16d79b
vitormirandajf <vitor.miranda@viannasempre.com.br>
2023-07-03 16:46:57 -0300

    implementação FileWriter e BufferedWriter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,22 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "c:\\temp\\test.txt";
+        String[] lines = new String[] {"Good morning","Good afternoon","Good night" };
+        String path = "c:\\temp\\outSaida.txt";
 
-
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-            String line = br.readLine();
-
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) { //Acrescentando um segundo parametro true, ele não recriará o arquivo e sim inserir apos as linhas
+            for(String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
-
-        }catch (IOException e) {
-            System.out.println("Erro: "+ e.getMessage());
+        }
+        catch (IOException e ) {
+            e.printStackTrace();
         }
 
     }
