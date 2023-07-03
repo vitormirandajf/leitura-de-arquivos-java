e622e645b941e6e18cd200c79fe50b4878e96745
vitormirandajf <vitor.miranda@viannasempre.com.br>
2023-07-03 16:34:26 -0300

    implementação resumida no bloco try do BufferedReader/FileReader

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path = "c:\\temp\\test.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
 
@@ -23,17 +20,6 @@ public class Main {
 
         }catch (IOException e) {
             System.out.println("Erro: "+ e.getMessage());
-        }finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
     }
